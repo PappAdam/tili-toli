d26e20fde1f88c108978701c66dab54debf00f1e
somloi.david <somloi.david@students.jedlik.eu>
2023-03-06 13:55:38 +0100

    halál

--- TolTIL/MainWindow.xaml.cs
+++ TolTIL/MainWindow.xaml.cs
@@ -20,23 +20,12 @@ namespace TolTIL
     /// </summary>
     public partial class MainWindow : Window
     {
-        Button[,] imgMat = new Button[4, 4];
+        Image[,] imgMat = new Image[4, 4];
         Button imgToSwapWith = null;
         public MainWindow()
         {
             InitializeComponent();
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    imgMat[i, j] = new Button();
-                    imgMat[i, j].Content = i * 4 + j;
-                    imgMat[i, j].MouseLeftButtonDown += new MouseButtonEventHandler(OnImgClick);
-                    Grid.SetColumn(imgMat[i, j], i);
-                    Grid.SetRow(imgMat[i, j], j);
-                    GRTili.Children.Add(imgMat[i, j]);
-                }
-            }
+            FillGrid();
         }
 
         private void OnImgClick(object sender, MouseButtonEventArgs e)
@@ -44,6 +33,43 @@ namespace TolTIL
             if (imgToSwapWith != null)
             {
                 
+            }
+        }
+
+        private void FillGrid()
+        {
+            int imgcount = 1;
+            int[] neighbours = { 1, -1, 4, -4};
+            Random rnd = new Random();
+            for (int i = 0; i <= 3; i++)
+            {
+                for (int j = 0; j <= 3; j++)
+                {
+                    imgMat[j, i] = new Image();
+                    imgMat[j, i].Source = new BitmapImage(new Uri($"/Images/jfk{imgcount}.png", UriKind.Relative));
+                    imgMat[j, i].MouseLeftButtonDown += new MouseButtonEventHandler(OnImgClick);
+                    Grid.SetColumn(imgMat[j, i], j);
+                    Grid.SetRow(imgMat[j, i], i);
+                    GRTili.Children.Add(imgMat[j, i]);
+                    imgcount++;
+                }
+            }
+            Image blank = GRTili.Children[15] as Image;
+            blank.Source = new BitmapImage(new Uri($"/Images/blank.png", UriKind.Relative));
+            Image image = blank;
+            for (int i = 0; i < 25; i++)
+            {
+                foreach (Image img in GRTili.Children)
+                {
+                    int rand = neighbours[rnd.Next(0, 4)];
+                    if (img.Source == blank.Source && i+rand > 0 && i+rand < 16)
+                    {
+                        image = img;
+                        Image ahh = GRTili.Children[i+rand] as Image;
+                        ahh.Source = blank.Source;
+                        blank.Source = image.Source;
+                    }
+                }
             }
         }
     }
