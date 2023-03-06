a0d3b1fc300ce12062c6aa255c33fba4f917dbe9
Papp Adam <papp.adam@students.jedlik.eu>
2023-03-06 13:57:10 +0100

    c6yidfgituskjghgfdfetgfruy7kji

--- TolTIL/MainWindow.xaml.cs
+++ TolTIL/MainWindow.xaml.cs
@@ -20,8 +20,8 @@ namespace TolTIL
     /// </summary>
     public partial class MainWindow : Window
     {
-        Button[,] imgMat = new Button[4, 4];
-        Button imgToSwapWith = null;
+        Rectangle[,] imgMat = new Rectangle[4, 4];
+        int[] placeholderTile = {0, 0};
         public MainWindow()
         {
             InitializeComponent();
@@ -29,9 +29,10 @@ namespace TolTIL
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    imgMat[i, j] = new Button();
-                    imgMat[i, j].Content = i * 4 + j;
+                    imgMat[i, j] = new Rectangle();
                     imgMat[i, j].MouseLeftButtonDown += new MouseButtonEventHandler(OnImgClick);
+                    imgMat[i, j].Fill = new SolidColorBrush(Color.FromRgb(Convert.ToByte(i * 20), 0, Convert.ToByte(j * 20)));
+                    imgMat[i, j].Tag = i * 4 + j;
                     Grid.SetColumn(imgMat[i, j], i);
                     Grid.SetRow(imgMat[i, j], j);
                     GRTili.Children.Add(imgMat[i, j]);
@@ -41,9 +42,41 @@ namespace TolTIL
 
         private void OnImgClick(object sender, MouseButtonEventArgs e)
         {
-            if (imgToSwapWith != null)
+
+            int[] senderCoords = { Grid.GetColumn((Rectangle)(sender)), Grid.GetRow((Rectangle)(sender))};
+            if (Math.Abs(senderCoords[0] - placeholderTile[0]) <= 1) {
+                if (Math.Abs(senderCoords[1] - placeholderTile[1]) <= 1)
+                {
+                    if ((senderCoords[1] - placeholderTile[1]) * (senderCoords[0] - placeholderTile[0]) == 0)
+                    {
+                        Grid.SetColumn((Rectangle)sender, placeholderTile[0]);
+                        Grid.SetRow((Rectangle)sender, placeholderTile[1]);
+                        Grid.SetColumn(imgMat[placeholderTile[0], placeholderTile[1]], senderCoords[0]);
+                        Grid.SetRow(imgMat[placeholderTile[0], placeholderTile[1]], senderCoords[1]);
+                        var temp = imgMat[placeholderTile[0], placeholderTile[1]];
+                        imgMat[placeholderTile[0], placeholderTile[1]] = imgMat[senderCoords[0], senderCoords[1]];
+                        imgMat[senderCoords[0], senderCoords[1]] = temp;
+                        placeholderTile[0] = senderCoords[0];
+                        placeholderTile[1] = senderCoords[1];
+                    }
+                }
+            }
+
+            bool a = false;
+            for (int y = 0; y < 4; y++)
             {
-                
+                for (int x = 0; x < 4; x++)
+                {
+                    if ((int)imgMat[y, x].Tag != Grid.GetColumn(imgMat[y, x]) * 4 + Grid.GetRow(imgMat[y, x]))
+                    {
+                        a = true;
+                    }
+                }
+            }
+
+            if (!a)
+            {
+                MessageBox.Show("asd");
             }
         }
     }
