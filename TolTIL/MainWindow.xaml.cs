053a8f6398a76a2ff59830a2f0ebf4dfc62001c8
Papp Adam <papp.adam@students.jedlik.eu>
2023-03-10 09:37:13 +0100

    Kesz vagy nem

--- TolTIL/MainWindow.xaml.cs
+++ TolTIL/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,7 @@ namespace TolTIL
     {
         Rectangle[,] imgMat = new Rectangle[4, 4];
         int[] placeholderTile = {0, 0};
+        Random rng = new Random();
         public MainWindow()
         {
             InitializeComponent();
@@ -30,27 +32,54 @@ namespace TolTIL
                 for (int j = 0; j < 4; j++)
                 {
                     imgMat[i, j] = new Rectangle();
-                    imgMat[i, j].MouseLeftButtonDown += new MouseButtonEventHandler(OnImgClick);
-                    imgMat[i, j].Fill = new SolidColorBrush(Color.FromRgb(Convert.ToByte(i * 20), 0, Convert.ToByte(j * 20)));
+                    imgMat[i, j].MouseLeftButtonDown += new MouseButtonEventHandler(OnTileClick);
+                    imgMat[i, j].Fill = new SolidColorBrush(Color.FromRgb(Convert.ToByte(i * 255/5), 0, Convert.ToByte(j * 255/5)));
                     imgMat[i, j].Tag = i * 4 + j;
                     Grid.SetColumn(imgMat[i, j], i);
                     Grid.SetRow(imgMat[i, j], j);
                     GRTili.Children.Add(imgMat[i, j]);
                 }
             }
+
+            Shuffle(100);
         }
 
-        private void OnImgClick(object sender, MouseButtonEventArgs e)
+        public void Shuffle(int depth)
         {
+            for (int i = 0; i < depth; i++)
+            {
+                int x = rng.Next(3) - 1;
+                if (x != 0) { 
+                    if (placeholderTile[0] == 0)
+                        x = 1;
+                    else if (placeholderTile[0] == 3)
+                        x = -1;
+                }
 
-            int[] senderCoords = { Grid.GetColumn((Rectangle)(sender)), Grid.GetRow((Rectangle)(sender))};
-            if (Math.Abs(senderCoords[0] - placeholderTile[0]) <= 1) {
+                int y = 0;
+                if (x == 0)
+                {
+                    y = rng.Next(2) * 2 - 1;
+                    if (placeholderTile[1] == 0)
+                        y = 1;
+                    else if (placeholderTile[1] == 3)
+                        y = -1;
+                }
+                SwapTiles(imgMat[placeholderTile[1] + y, placeholderTile[0] + x]);
+            }
+        }
+
+        public void SwapTiles(UIElement sender)
+        {
+            int[] senderCoords = { Grid.GetColumn((sender)), Grid.GetRow((sender)) };
+            if (Math.Abs(senderCoords[0] - placeholderTile[0]) <= 1)
+            {
                 if (Math.Abs(senderCoords[1] - placeholderTile[1]) <= 1)
                 {
                     if ((senderCoords[1] - placeholderTile[1]) * (senderCoords[0] - placeholderTile[0]) == 0)
                     {
-                        Grid.SetColumn((Rectangle)sender, placeholderTile[0]);
-                        Grid.SetRow((Rectangle)sender, placeholderTile[1]);
+                        Grid.SetColumn(sender, placeholderTile[0]);
+                        Grid.SetRow(sender, placeholderTile[1]);
                         Grid.SetColumn(imgMat[placeholderTile[0], placeholderTile[1]], senderCoords[0]);
                         Grid.SetRow(imgMat[placeholderTile[0], placeholderTile[1]], senderCoords[1]);
                         var temp = imgMat[placeholderTile[0], placeholderTile[1]];
@@ -61,7 +90,11 @@ namespace TolTIL
                     }
                 }
             }
+        }
 
+        private void OnTileClick(object sender, MouseButtonEventArgs e)
+        {
+            SwapTiles((UIElement)sender);
             bool a = false;
             for (int y = 0; y < 4; y++)
             {
@@ -76,7 +109,7 @@ namespace TolTIL
 
             if (!a)
             {
-                MessageBox.Show("asd");
+                MessageBox.Show("Ki lett rakva jej btw a kurva anyd");
             }
         }
     }
